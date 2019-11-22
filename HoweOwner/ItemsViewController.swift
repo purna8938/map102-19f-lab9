d54f36e025faa366194ce160a20dd553e47cd41d
purna sai kiran <purnasaikiran.ponnam@gmail.com>
2019-11-22 16:17:44 -0500

    lab9 completed

--- HoweOwner/ItemsViewController.swift
+++ HoweOwner/ItemsViewController.swift
@@ -109,4 +109,19 @@ class ItemsViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If the triggered segue is the "showItem" segue
+        switch segue.identifier {
+        case "showItem"?:
+            // Figure out which row was just tapped
+            if let row = tableView.indexPathForSelectedRow?.row {
+                // Get the item associated with this row and pass it along
+                let item = itemStore.allItems[row]
+                let detailViewController
+                    = segue.destination as! DetailViewController
+                detailViewController.item = item
+            } default:
+                preconditionFailure("Unexpected segue identifier.")
+        }
+    }
 }
